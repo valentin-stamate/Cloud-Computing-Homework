f73d28a7fe29f18f6af116ca664c3d242f02cbb4
Stamate Valentin <stamatevalentin125@gmail.com>
2022-04-16 08:12:46 +0300

    base db connection

---
+++ homework 04/backend/src/database.ts
@@ -0,0 +1,27 @@
+import {Container, CosmosClient, Database} from "@azure/cosmos";
+require('dotenv').config();
+
+const env = process.env;
+
+const endpoint = env.DB_ENDPOINT as string;
+const key = env.DB_KEY as string;
+const databaseId = env.DB_ID;
+const containerId = env.DB_CONTAINER_ID;
+
+const client = new CosmosClient({
+    endpoint: endpoint,
+    key: key
+});
+
+let db: Database = {} as Database;
+let cont: Container = {} as Container;
+
+export async function main() {
+    const {database} = await client.databases.createIfNotExists({id: databaseId});
+    const {container} = await database.containers.createIfNotExists({id: containerId});
+
+    db = database;
+    cont = container;
+
+    console.log('Successfully connected with Azure Cosmos DB');
+}
--- homework 04/backend/src/server.ts
+++ homework 04/backend/src/server.ts
@@ -1,8 +1,13 @@
 import express = require("express");
+import {main} from "./database";
 
 const app = express();
 const port = 8080;
 const host = `http://localhost:${port}`
+
+main().catch(err => {
+    console.error(err);
+});
 
 app.get('/', (req, res) => {
     res.end('Hello word!')
