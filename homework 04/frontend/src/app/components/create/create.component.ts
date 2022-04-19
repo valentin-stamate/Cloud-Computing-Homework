016d12999f904196447f1b65d432bb08f49be1f0
Stamate Valentin <stamatevalentin125@gmail.com>
2022-04-19 17:41:25 +0300

    ingredients ngfor

--- homework 04/frontend/src/app/components/create/create.component.ts
+++ homework 04/frontend/src/app/components/create/create.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {RecipeItem, RecipesPost} from "../../service/models";
 import axios from "axios";
 import {Endpoints} from "../../service/endpoints";
-import { Router } from '@angular/router';
-import {UtilService} from "../../service/util.service";
-import { Buffer } from "buffer";
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -27,6 +24,9 @@ export class CreateComponent implements OnInit {
 
     const formData = new FormData(form);
     formData.append("items", JSON.stringify(this.arrayOfingredients));
+
+    // const formObject = Object.fromEntries(formData);
+
     axios.post(Endpoints.POSTS, formData)
       .then(res => {
 
