87a7526ada9c6790c86e3d3cf668d51d7aee4083
Stamate Valentin <stamatevalentin125@gmail.com>
2022-05-08 18:23:00 +0300

    base config

---
+++ project/backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+const host = `http://localhost:${port}`
+
+app.get('/', (req, res) => {
+    res.end('Hello word!')
+});
+
+app.listen(port, () => {
+    console.log(`Server started at ${host}`);
+});
