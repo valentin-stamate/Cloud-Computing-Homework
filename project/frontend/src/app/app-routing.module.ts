adc6f2dbf871265800bb095d84538ac4e78eb6b1
Stamate Valentin <stamatevalentin125@gmail.com>
2022-05-14 17:35:45 +0300

    restaurant page layout

--- project/frontend/src/app/app-routing.module.ts
+++ project/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RestaurantComponent} from "./components/restaurant/restaurant.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'restaurant', component: RestaurantComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/frontend/src/app/app.module.ts
+++ project/frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
