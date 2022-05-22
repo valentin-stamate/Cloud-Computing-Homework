631da40f3eca4a9fa3e048b1e6616a81c7a89f8d
Stamate Valentin <stamatevalentin125@gmail.com>
2022-05-22 16:50:53 +0300

    navbar integration(almost)

--- project/frontend/src/app/components/navbar/navbar.component.ts
+++ project/frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Cookies, CookieService} from "../../service/cookie.service";
+import {AxiosRequestConfig} from "axios";
+import {JwtService} from "../../service/jwt.service";
+import {FoodItem, Restaurant} from "../../service/models";
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  config: AxiosRequestConfig = {} as any;
+  userType: number = 0;
+  logged = false;
+  searchedFood: FoodItem[] = [];
+  searchBoxOpen = false;
+
+  user: any = {};
+
+  constructor() {
+    const token = CookieService.readCookie(Cookies.AUTH);
+
+    if (!token) {
+      return;
+    }
+
+    this.logged = true;
+    const decodedToken = JwtService.decodeJWT(token);
+
+    this.user = decodedToken;
+    this.userType = decodedToken.userType;
+
+    this.config = {
+      headers: {
+        'Authorization': token
+      }
+    };
+  }
 
   ngOnInit(): void {
+  }
+
+  onSearch(searchInput: HTMLInputElement) {
+    const searchValue = searchInput.value;
+    this.searchBoxOpen = true;
+
+    /* Demo */
+
+    const foodItem = {
+      id: 1,
+      name: 'Ciorba',
+      price: 31,
+      details: 'dasdd',
+      photoUrl: 'https://retete-culinare-cu-dana-valery.ro/cdn/recipes/ciorba-radauteana-cu-carne-de-curcan.jpg',
+      restaurant: {} as Restaurant,
+    };
+
+    this.searchedFood.push(foodItem);
+    this.searchedFood.push(foodItem);
+    this.searchedFood.push(foodItem);
+  }
+
+  onSearchClose() {
+    this.searchBoxOpen = false;
   }
 
 }
