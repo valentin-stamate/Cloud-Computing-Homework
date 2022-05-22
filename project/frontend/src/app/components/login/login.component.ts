715c95313734f801a92043c00bdbdb0dc66d8de8
Stamate Valentin <stamatevalentin125@gmail.com>
2022-05-22 09:54:46 +0300

    auth ui integration

--- project/frontend/src/app/components/login/login.component.ts
+++ project/frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import axios from "axios";
+import {Endpoints} from "../../service/endpoints";
+import {Cookies, CookieService} from "../../service/cookie.service";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginWithCode = false;
+
+  authType = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(form: HTMLFormElement) {
+
+    const formData = new FormData(form);
+
+    if (!this.loginWithCode) {
+      axios.post(Endpoints.USER_LOGIN, formData)
+        .then(res => {
+          this.loginWithCode = true;
+        }).catch(err => {
+          console.log(err);
+        });
+
+      return;
+    }
+
+    axios.post(Endpoints.USER_LOGIN_CODE, formData)
+      .then(res => {
+        CookieService.setCookie(Cookies.AUTH, res.data);
+        window.location.href = '/home';
+      }).catch(err => {
+
+    });
+
+  }
+
+  onLoginRestaurant(form: HTMLFormElement) {
+    const formData = new FormData(form);
+
+    if (!this.loginWithCode) {
+      axios.post(Endpoints.RESTAURANT_LOGIN, formData)
+        .then(res => {
+          this.loginWithCode = true;
+        }).catch(err => {
+        console.log(err);
+      });
+
+      return;
+    }
+
+    axios.post(Endpoints.RESTAURANT_LOGIN_CODE, formData)
+      .then(res => {
+        CookieService.setCookie(Cookies.AUTH, res.data);
+        window.location.href = '/home';
+      }).catch(err => {
+
+    });
+  }
+
+  changeAuthType(authType: number) {
+    this.authType = authType;
   }
 
 }
--- project/frontend/src/app/components/signup/signup.component.ts
+++ project/frontend/src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import axios from "axios";
+import {Endpoints} from "../../service/endpoints";
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  authType = 1;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onUserSignup(form: HTMLFormElement) {
+
+    const formData = new FormData(form);
+
+    axios.post(Endpoints.USER_SIGNUP, formData)
+      .then(res => {
+        location.href = '/login';
+      }).catch(err => {
+        console.log(err);
+      });
+
+  }
+
+  onRestaurantSignup(form: HTMLFormElement) {
+
+    const formData = new FormData(form);
+
+    axios.post(Endpoints.RESTAURANT_SIGNUP, formData)
+      .then(res => {
+        location.href = '/login';
+      }).catch(err => {
+      console.log(err);
+    });
+
+  }
+
+
+
+  changeAuthType(authType: number) {
+    this.authType = authType;
+  }
+
 
 }
